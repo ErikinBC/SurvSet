b84d2efa3476868510df43a601ea1442a9c5c6d8
Erik Drysdale <alla_hack@hotmail.com>
2022-03-09 10:58:01 -0500

    fixing classifier issue

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
-        'OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: POSIX :: Linux',        
         'Programming Language :: Python :: 3',
     ],
