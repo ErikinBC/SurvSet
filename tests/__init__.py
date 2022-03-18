a63c2da2f2711b913db26944c5c63c9b7de663e2
Erik Drysdale <alla_hack@hotmail.com>
2022-03-18 09:05:36 -0400

    making small change to __init__

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,7 +7,7 @@
 # # on some test conda env
 # twine upload --repository-url https://test.pypi.org/legacy/ dist/SurvSet*
 # pip uninstall SurvSet
-# pip install --index-url https://test.pypi.org/simple/ pyexample --user
+# pip install --index-url https://test.pypi.org/simple/ SurvSet --user
 
 # # Upload to PyPI: https://pypi.org/project/SurvSet/
 # twine upload dist/SurvSet*
